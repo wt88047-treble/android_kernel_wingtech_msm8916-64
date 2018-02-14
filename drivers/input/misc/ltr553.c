LineageOS/android_kernel_cyanogen_msm8916

input: sensors: ltr553: Allow maximum number of LED pulses
* LTR sensor appears to allow a maximum of up to 0xF pulses, so let
  this be the max entered from the device tree.

REF: KIPPER-428
Change-Id: I866be5c196230b4967dc02f2db58c874970a8f28

drivers/input/misc/ltr553.c
@@ -437,7 +437,7 @@ static int ltr553_parse_dt(struct device *dev, struct ltr553_data *ltr)
 		dev_err(dev, "read liteon,ps-pulses failed\n");
 		return rc;
 	}
-	if (value > 0x7) {
+	if (value > 0x10) {
 		dev_err(dev, "liteon,ps-pulses out of range\n");
 		return -EINVAL;
 	}

wt88047-treble/android_kernel_wingtech_msm8916-64

drivers/input/misc/ltr553.c
@@ -449,7 +449,7 @@ static int ltr553_parse_dt(struct device *dev, struct ltr553_data *ltr)
 		dev_err(dev, "read liteon,ps-pulses failed\n");
 		return rc;
 	}
-	if (value > 0x7) {
+	if (value > 0xf) {
 		dev_err(dev, "liteon,ps-pulses out of range\n");
 		return -EINVAL;
 	}